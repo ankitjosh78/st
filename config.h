4b8dcd4a3f63c6715a2f81192429800183cdb01f
Ankit Josh <ankitjosh78@protonmail.com>
2021-06-22 21:43:02 +0530

    Colorscheme using pywal.

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Nerd Font:pixelsize=17:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Nerd Font:pixelsize=16:antialias=true:autohint=true";
 /*spare fonts*/
 static char *font2[] = { "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static char *font3[] = { "FontAwesome:pixelsize=12:antialias=true:autohint=true" };
@@ -99,39 +99,10 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 1.0;
-
+float alpha = 0.8;
+#include "/home/ankit/.cache/wal/colors-wal-st.h"
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-
-  /* 8 normal colors */
-  [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-  [1] = "#cc241d", /* red     */
-  [2] = "#98971a", /* green   */
-  [3] = "#d79921", /* yellow  */
-  [4] = "#458588", /* blue    */
-  [5] = "#b16286", /* magenta */
-  [6] = "#689d6a", /* cyan    */
-  [7] = "#a89984", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#928374", /* black   */
-  [9]  = "#fb4934", /* red     */
-  [10] = "#b8bb26", /* green   */
-  [11] = "#fabd2f", /* yellow  */
-  [12] = "#83a598", /* blue    */
-  [13] = "#d3869b", /* magenta */
-  [14] = "#8ec07c", /* cyan    */
-  [15] = "#ebdbb2", /* white   */
-}; 
 /*
- * Default colors (colorname index)
- * foreground, background, cursor
- */
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
-static unsigned int defaultrcs = 257;/*
 
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
  * will reverse too. Another logic would only make the simple feature too
