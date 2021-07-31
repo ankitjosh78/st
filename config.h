59cd72b4d55a62b6ea784ef9d281c3cec8195de0
Ankit Josh <ankitjosh78@protonmail.com>
2021-07-31 18:49:35 +0530

    Font Size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Medium:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Medium:pixelsize=15:antialias=true:autohint=true";
 /*spare fonts*/
 static char *font2[] = { "JoyPixels:pixelsize=13:antialias=true:autohint=true" };
 static char *font3[] = { "FontAwesome:pixelsize=13:antialias=true:autohint=true" };
@@ -97,10 +97,35 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
-
-/* bg opacity */
-float alpha = 0.7;
+//static const char *colorname[] = {
+//  /* 8 normal colors */
+//  [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+//  [1] = "#cc241d", /* red     */
+//  [2] = "#98971a", /* green   */
+//  [3] = "#d79921", /* yellow  */
+//  [4] = "#458588", /* blue    */
+//  [5] = "#b16286", /* magenta */
+//  [6] = "#689d6a", /* cyan    */
+//  [7] = "#a89984", /* white   */
+//
+//  /* 8 bright colors */
+//  [8]  = "#928374", /* black   */
+//  [9]  = "#fb4934", /* red     */
+//  [10] = "#b8bb26", /* green   */
+//  [11] = "#fabd2f", /* yellow  */
+//  [12] = "#83a598", /* blue    */
+//  [13] = "#d3869b", /* magenta */
+//  [14] = "#8ec07c", /* cyan    */
+//  [15] = "#ebdbb2", /* white   */
+//};
+///* bg opacity */
+//unsigned int defaultfg = 15;
+//unsigned int defaultbg = 0;
+//static unsigned int defaultcs = 15;
+//static unsigned int defaultrcs = 257;
+float alpha = 0.8;
 #include "/home/ankit/.cache/wal/colors-wal-st.h"
+
 /* Terminal colors (16 first used in escape sequence) */
 /*
 
