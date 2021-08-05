0c96ad1536bccb7807491e42fe6b0c72eed9f7a4
Ankit Josh <ankitjosh78@protonmail.com>
2021-08-05 21:40:09 +0530

    Font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Medium:pixelsize=15:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Medium:pixelsize=14:antialias=true:autohint=true";
 /*spare fonts*/
 static char *font2[] = { "JoyPixels:pixelsize=13:antialias=true:autohint=true" };
 static char *font3[] = { "FontAwesome:pixelsize=13:antialias=true:autohint=true" };
